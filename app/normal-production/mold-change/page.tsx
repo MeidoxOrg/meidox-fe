af9c025c33c344679c9f28a7e4672d15b4c673cd
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-01 12:36:40 +0700

    feat(@ normal-production/mold-change): add api create

--- app/normal-production/mold-change/page.tsx
+++ app/normal-production/mold-change/page.tsx
@@ -4,14 +4,38 @@ import { useRouter } from "next/navigation"
 import { PageLayout } from "@/components/layout/page-layout"
 import { WorkInputForm } from "@/components/common/WorkInputForm"
 import { WorkInputFormValues } from "@/model/custom"
+import workSessionMoldChangeServies from "@/services/work-session-mold-change"
+import { localStorageService } from "@/helper/localstorage"
+import { WORKSESSION_ID, WORKSESSION_MOLD_CHANGE_ID } from "@/utils/constants"
 
 
 export default function MoldChange() {
     const router = useRouter()
 
-    const onSubmit = (data: WorkInputFormValues) => {
+    const onSubmit = async (data: WorkInputFormValues) => {
         console.log(data);
-        //router.push("/normal-production/mold-change-progress")
+        try {
+            const now = new Date()
+            const currentDate = now.toISOString().split("T")[0]
+            const currentTime = now.toTimeString().slice(0, 5)
+            const workSessionId = localStorageService.get<string>(WORKSESSION_ID, "")
+
+            const response = await workSessionMoldChangeServies.createWorkSessionMoldChange({
+                dateStart: currentDate,
+                timeStart: currentTime,
+                lotNumber: data.lotNumber,
+                materialNumber: data.materialNumber,
+                productNumber: data.productCode,
+                workSessionId: workSessionId
+            })
+
+            if (response.id) {
+                localStorageService.set<String>(WORKSESSION_MOLD_CHANGE_ID, response.id)
+                router.push("/normal-production/mold-change-progress")
+            }
+        } catch (error) {
+
+        }
     }
 
     return (
--- utils/constants.ts
+++ utils/constants.ts
@@ -3,3 +3,4 @@ export const WORKSESSION_ID = "workSessionId";
 export const WORKSESSION_SETUP_ID = "workSessionSetupId";
 export const WORKSESSION_PRODUCTION_ID = "workSessionProductionId";
 export const PRODUCT_INFO = "productInfo";
+export const WORKSESSION_MOLD_CHANGE_ID = "workSessionMoldChangeId";

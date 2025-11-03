c28f51a773e84cb6e5ad84444ca0edf1b120fb09
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-03 21:28:40 +0700

    feat(@ normal-production/material-change-progress): move to normal-production

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -22,7 +22,7 @@ export default function Dashboard() {
         { label: "生産準備\n暖機運転\n保全チェック", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/production-prep-check" },
         { label: "選別", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/sorting" },
         { label: "その他停止", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/other-stop" },
-        { label: "材料交換", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/material-change" },
+        { label: "材料交換", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/material-change" },
         { label: "他機対応", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/other-machine-support" },
         { label: "品質チェック", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/quality-check" },
         { label: "", color: "invisible", route: "#" },

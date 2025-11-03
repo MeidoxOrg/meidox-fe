ce65d59206b597a5e16689d0c33333fc382266cb
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-03 22:23:31 +0700

    feat(@ normal-production/adjustment-begins): done

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,3 +5,5 @@ export const WORKSESSION_PRODUCTION_ID = "workSessionProductionId";
 export const PRODUCT_INFO = "productInfo";
 export const WORKSESSION_MOLD_CHANGE_ID = "workSessionMoldChangeId";
 export const WORKSESSION_MATERIAL_CHANGE_ID = "workSessionMaterialChangeId";
+export const WORKSESSION_ADJUSTMENT_BEGIN_CHANGE_ID =
+  "workSessionAdjustmentBeginId";

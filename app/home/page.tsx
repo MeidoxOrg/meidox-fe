4582cd6e1ba6085f6ddd49d2ac5099446798c63c
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-04 21:45:30 +0700

    normal-production/production-prep-progress ): move to normal-productionfeat(@

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -19,7 +19,7 @@ export default function Dashboard() {
         { label: "4S", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/4S" },
         { label: "給油", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/refuel" },
         { label: "金型交換", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/mold-change" },
-        { label: "生産準備\n暖機運転\n保全チェック", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/production-prep-check" },
+        { label: "生産準備\n暖機運転\n保全チェック", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/production-prep-check" },
         { label: "選別", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/sorting" },
         { label: "その他停止", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/other-stop" },
         { label: "材料交換", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/material-change" },

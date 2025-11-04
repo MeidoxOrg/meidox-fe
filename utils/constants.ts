2f496748439ed3a981abb5177567cd50bb49af49
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-04 21:12:15 +0700

    feat(@ normal-production/4S): done

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,3 +7,4 @@ export const WORKSESSION_MOLD_CHANGE_ID = "workSessionMoldChangeId";
 export const WORKSESSION_MATERIAL_CHANGE_ID = "workSessionMaterialChangeId";
 export const WORKSESSION_ADJUSTMENT_BEGIN_CHANGE_ID =
   "workSessionAdjustmentBeginId";
+export const WORKSESSION_4S_ID = "workSession4SId";

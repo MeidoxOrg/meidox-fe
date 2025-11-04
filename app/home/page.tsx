b2117f71d0353e7cb9d24f00a904a892798e929e
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-04 20:55:12 +0700

    feat(@ normal-production/4S): move to folder normal

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -16,7 +16,7 @@ export default function Dashboard() {
     const mainGridButtons = [
         { label: "段取り", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/setup-start" },
         { label: "調整", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/adjustment-begins" },
-        { label: "4S", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/4S" },
+        { label: "4S", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/4S" },
         { label: "給油", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/refuel" },
         { label: "金型交換", color: "bg-blue-500 hover:bg-blue-600", disable: false, route: "/normal-production/mold-change" },
         { label: "生産準備\n暖機運転\n保全チェック", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/production-prep-check" },

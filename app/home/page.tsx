5a00c29ffaf0f64e03b0643355936f70f5a6d965
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-05 20:42:27 +0700

    feat(@ normal-production/equipment-repair-progress): move to normal production

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -27,7 +27,7 @@ export default function Dashboard() {
         { label: "品質チェック", color: "bg-blue-500 hover:bg-blue-600", disable: true, route: "/quality-check" },
         { label: "", color: "invisible", route: "#" },
         { label: "異常処置", color: "bg-red-500 hover:bg-red-600", disable: true, route: "/abnormal-handling" },
-        { label: "設備故障\n設備修理", color: "bg-red-500 hover:bg-red-600", disable: true, route: "/equipment-repair" },
+        { label: "設備故障\n設備修理", color: "bg-red-500 hover:bg-red-600", disable: false, route: "/normal-production/equipment-repair" },
         { label: "無人運転\n(昼休憩)", color: "bg-green-600 hover:bg-green-700", disable: true, route: "/unmanned-lunch" },
         { label: "無人運転\n(残業)", color: "bg-green-600 hover:bg-green-700", disable: true, route: "/unmanned-operation-overtime" },
     ]

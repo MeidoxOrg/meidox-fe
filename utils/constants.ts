8626dae82e76b4222958684e0e9acc3c836d8e3a
ThanhTrong29 <203505393+ThanhTrong29@users.noreply.github.com>
2025-11-06 19:58:09 +0700

    feat(@ reason-for-stopping/break-start): done

--- utils/constants.ts
+++ utils/constants.ts
@@ -16,3 +16,4 @@ export const WORKSESSION_EQUIPMENT_REPAIR_ID = "workSessionEquipmentRepairId";
 export const WORKSESSION_ORTHER_MACHINES_SUPPORT_ID =
   "workSessionOtherMachineSupportId";
 export const WORKSESSION_QUANLITY_CHECK_ID = "workSessionQuanlityCheckId";
+export const REASON_FOR_STOPPING_BREAK_START = "reasonForStoppingBreakStartId";
